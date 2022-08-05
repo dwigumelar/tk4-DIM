53b26122b84e2f5215440471f66de317213cdc66
dwigumelar <dwi47gumelar@gmail.com>
2022-08-05 17:40:36 +0700

    Add files via upload

---
+++ database/migrations/2022_08_05_023710_create_pegawais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->increments('id_pegawai');
+            $table->string('nama_pegawai');
+            $table->text('alamat_pegawai');
+            $table->string('hp_pegawai');
+            $table->string('jabatan_pegawai');
+            $table->integer('id_bagian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
